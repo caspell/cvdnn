09ee2ac0f01fdae336eea1ed091fd86cc4af3124
caspell <eo2e89oo>
2017-03-19 20:24:06 +0900

    lmdb creat

--- opt_flow.py
+++ opt_flow.py
@@ -7,7 +7,7 @@ USAGE: opt_flow.py [<video_source>]
 
 Keys:
  1 - toggle HSV flow visualization
- 2 - toggle glitch
+ 2 - toggle
 
 Keys:
     ESC    - exit
@@ -18,7 +18,7 @@ from __future__ import print_function
 
 import numpy as np
 import cv2
-import video
+import cassandra
 
 def draw_flow(img, flow, step=16):
     h, w = img.shape[:2]
@@ -32,7 +32,6 @@ def draw_flow(img, flow, step=16):
         cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
     return vis
 
-
 def draw_hsv(flow):
     h, w = flow.shape[:2]
     fx, fy = flow[:,:,0], flow[:,:,1]
@@ -45,52 +44,100 @@ def draw_hsv(flow):
     bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
     return bgr
 
+def draw_flow_lines (img, flow, step=16):
+    h, w = img.shape[:2]
 
-def warp_flow(img, flow):
-    h, w = flow.shape[:2]
-    flow = -flow
-    flow[:,:,0] += np.arange(w)
-    flow[:,:,1] += np.arange(h)[:,np.newaxis]
-    res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
-    return res
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
+
+    fx, fy = flow[y,x].T
+
+    lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
+
+    lines = np.int32(lines + 0.5)
+
+    vis = np.zeros((h, w, 3), np.uint8)
+
+    cv2.polylines(vis, lines, 0, (0, 255, 0))
+    for (x1, y1), (x2, y2) in lines:
+        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
+
+
+kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+
+IMAGE_SHARPEN_NUM = -1
+
+def imageSharpening (frame) :
+    return cv2.filter2D(frame, IMAGE_SHARPEN_NUM, kernel)
 
 if __name__ == '__main__':
     import sys
     print(__doc__)
-    try:
-        fn = sys.argv[1]
-    except IndexError:
-        fn = 0
 
-    cam = video.create_capture(fn)
+    fn = '/home/mhkim/data/video/sampho2.mp4'
+
+    # try:
+    #     fn = sys.argv[1]
+    # except IndexError:
+    #     fn = 0
+
+    # cam = video.create_capture(fn)
+    cam = cv2.VideoCapture(fn)
+
     ret, prev = cam.read()
+
+    prev = imageSharpening(prev)
+
     prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
     show_hsv = False
-    show_glitch = False
+    show_lines = False
     cur_glitch = prev.copy()
 
+    frameCount = 0
+
     while True:
+
         ret, img = cam.read()
+
+        img = imageSharpening(img)
+
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
         flow = cv2.calcOpticalFlowFarneback(prevgray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
         prevgray = gray
 
+        # font = cv2.FONT_HERSHEY_SIMPLEX
+        # cv2.putText(gray, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+
         cv2.imshow('flow', draw_flow(gray, flow))
+
+
         if show_hsv:
             cv2.imshow('flow HSV', draw_hsv(flow))
-        if show_glitch:
-            cur_glitch = warp_flow(cur_glitch, flow)
-            cv2.imshow('glitch', cur_glitch)
+        if show_lines :
+            cv2.imshow('flow flow lines', draw_flow_lines(gray, flow))
+        # cv2.putText()
 
         ch = cv2.waitKey(5)
+
+        # print ( 'IMAGE_SHARPEN_NUM : ' , IMAGE_SHARPEN_NUM )
+
+        if ch == 82 :
+            #up
+            IMAGE_SHARPEN_NUM = IMAGE_SHARPEN_NUM + 1
+            if IMAGE_SHARPEN_NUM > -1 :
+                IMAGE_SHARPEN_NUM = -1
+        if ch == 84 :
+            #down
+            IMAGE_SHARPEN_NUM = IMAGE_SHARPEN_NUM - 1
         if ch == 27:
             break
         if ch == ord('1'):
             show_hsv = not show_hsv
             print('HSV flow visualization is', ['off', 'on'][show_hsv])
         if ch == ord('2'):
-            show_glitch = not show_glitch
-            if show_glitch:
-                cur_glitch = img.copy()
-            print('glitch is', ['off', 'on'][show_glitch])
+            show_lines = not show_lines
+            print('flow line visualization is', ['off', 'on'][show_lines])
+
     cv2.destroyAllWindows()
--- run2.py
+++ run2.py
@@ -20,34 +20,10 @@ lk_params = dict( winSize  = (15,15),
                   maxLevel = 2,
                   criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# Create some random colors
-color = np.random.randint(0,255,(100,3))
-# Take first frame and find corners in it
-ret, old_frame = cap.read()
-
-imgCrop = (lambda f : f[35:] )
-
-old_frame = imgCrop(old_frame)
-
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-# Create a mask image for drawing purposes
-mask = np.zeros_like(old_frame)
-
-while(1):
-
-    ret,frame = cap.read()
-
-    frame = imgCrop(frame)
-
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+def draw_flow1(old_gray, frame_gray, p0 , mask):
 
     # calculate optical flow
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-
-    if p1 is None :
-        break
 
     # Select good points
     good_new = p1[st==1]
@@ -62,23 +38,80 @@ while(1):
 
         # frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
 
-    # print ( np.shape(frame) )
-    # print (np.shape(mask))
-
-    # break
-
     img = cv2.add(frame , mask)
 
-    cv2.imshow('frame',img)
+    cv2.imshow('frame' , img)
 
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
 
     # Now update the previous frame and previous points
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1,1,2)
 
-    time.sleep(0.2)
+    return ( old_gray , p0 )
+
+def draw_flow2(img, flow, step=16):
+    h, w = img.shape[:2]
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
+    fx, fy = flow[y,x].T
+    lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    cv2.polylines(vis, lines, 0, (0, 255, 0))
+    for (x1, y1), (x2, y2) in lines:
+        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
+
+# Create some random colors
+color = np.random.randint(0,255,(100,3))
+# Take first frame and find corners in it
+ret, old_frame = cap.read()
+
+imgCrop = (lambda f : f[35:] )
+
+kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+
+def imageSharpening (frame) :
+    return cv2.filter2D(frame, -1, kernel)
+
+old_frame = imgCrop(old_frame)
+
+
+
+old_frame = imageSharpening(old_frame)
+
+
+old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+
+p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+# Create a mask image for drawing purposes
+mask = np.zeros_like(old_frame)
+
+while(1):
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+    ret,frame = cap.read()
+
+    frame = imgCrop(frame)
+
+    frame = imageSharpening(frame)
+
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    # optical flow - test 1
+    # old_gray , p0 = draw_flow1(old_gray, frame_gray, p0, mask)
+
+
+    # optical flow - test 2
+    flow = cv2.calcOpticalFlowFarneback(old_gray, frame_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+    cv2.imshow('flow', draw_flow2(frame_gray, flow))
+
+
+
+    # time.sleep(0.2)
 cv2.destroyAllWindows()
 cap.release()
